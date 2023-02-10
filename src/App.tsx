5393aba8d07f04baf2fd452bbc868c29f51f4225
Fedimar Kayl Rabanzo <fedimar.rabanzo@play-asia.com>
2023-02-10 17:55:22 +0800

    added tailwind style

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import FormsAndEventsTS from "./components/FormsAndEventsTS";
 import ExampleTS from "./components/UseSateTS";
 
 function App() {
-    return <div>
+    return <div className="m-10">
         {/* <ExampleTS /> */}
         <FormsAndEventsTS />
     </div>
--- src/components/FormsAndEventsTS.tsx
+++ src/components/FormsAndEventsTS.tsx
@@ -7,7 +7,7 @@ const defaultFormData = {
 
 function FormsAndEventsTS() {
 
-    const [formData, setFormData] = useState(defaultFormData)
+    const [formData, setFormData] = useState(defaultFormData);
     const { title, description } = formData;
 
     const handlePostSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -25,21 +25,24 @@ function FormsAndEventsTS() {
     return (
         <div>
             <form onSubmit={handlePostSubmit}>
-                <h1>Create a post</h1>
 
-                <div>
-                    <label htmlFor="title">Title</label>:
-                    <input type="text" name="title" id="title" value={title} onChange={onChange} />
+                <div className="grid gap-6 mb-6 md:grid-cols-1">
+                    <h1 className="text-4xl">Create a post</h1>
+                    <div>
+                        <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900">Title</label>
+                        <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" name="title" id="title" value={title} onChange={onChange} />
+                    </div>
+
+                    <div>
+                        <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900">Description</label>
+                        <input type="text" name="description" id="description" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2.5" value={description} onChange={onChange} />
+                    </div>
+
+                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" name="submit">
+                        Post
+                    </button>
                 </div>
 
-                <div>
-                    <label htmlFor="description">Description</label>:
-                    <input type="text" name="description" id="description" value={description} onChange={onChange} />
-                </div>
-
-                <button type="submit" name="submit">
-                    Post
-                </button>
             </form>
         </div>
     );
