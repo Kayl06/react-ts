6dae64d81ed30e7427be0d96ca374802858508f4
Kayl Rabanzo <frabanzoo@gmail.com>
2024-02-20 19:41:49 +0800

    test

--- src/components/FormsAndEventsTS.tsx
+++ src/components/FormsAndEventsTS.tsx
@@ -13,6 +13,8 @@ function FormsAndEventsTS() {
     const handlePostSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log('form submitted', formData);
+
+        setFormData(defaultFormData);
     }
 
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -30,12 +32,12 @@ function FormsAndEventsTS() {
                     <h1 className="text-4xl">Create a post</h1>
                     <div>
                         <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900">Title</label>
-                        <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" name="title" id="title" value={title} onChange={onChange} />
+                        <input placeholder="Enter your title" type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" name="title" id="title" value={title} onChange={onChange} />
                     </div>
 
                     <div>
                         <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900">Description</label>
-                        <input type="text" name="description" id="description" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2.5" value={description} onChange={onChange} />
+                        <input placeholder="Enter your description..." type="text" name="description" id="description" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2.5" value={description} onChange={onChange} />
                     </div>
 
                     <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" name="submit">
